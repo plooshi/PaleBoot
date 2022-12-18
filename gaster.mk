466fe708afb24d33da71afb40af8b30f87cc69cf
Tom E <guacaplushy@icloud.com>
2022-12-17 20:20:02 -0500

    whar the

--- gaster.mk
+++ gaster.mk
@@ -1,7 +1,6 @@
 all: gaster
 
 gaster:
-	cd deps/gaster
 	cat Makefile | sed 's/-o gaster/-o libgaster.a/' | tee Makefile > /dev/null
 	cat gaster.c | sed 's/main/gaster_main/' | sed 's/static //' | sed 's/unsigned usb_timeout;/unsigned usb_timeout = 5;/' | tee gaster.c > /dev/null
 	make libusb -C deps/gaster
@@ -10,4 +9,3 @@ gaster:
 	xxd -iC payload_notA9_armv7.bin payload_notA9_armv7.h
 	xxd -iC payload_handle_checkm8_request.bin payload_handle_checkm8_request.h
 	xxd -iC payload_handle_checkm8_request_armv7.bin payload_handle_checkm8_request_armv7.h
-	touch .compiled
