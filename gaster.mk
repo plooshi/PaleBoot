b6934369b3b49d696d6a8d081800c9812b737f09
Tom E <guacaplushy@icloud.com>
2022-12-17 20:20:36 -0500

    current dir: ratio

--- gaster.mk
+++ gaster.mk
@@ -3,7 +3,7 @@ all: gaster
 gaster:
 	cat Makefile | sed 's/-o gaster/-o libgaster.a/' | tee Makefile > /dev/null
 	cat gaster.c | sed 's/main/gaster_main/' | sed 's/static //' | sed 's/unsigned usb_timeout;/unsigned usb_timeout = 5;/' | tee gaster.c > /dev/null
-	make libusb -C deps/gaster
+	make libusb
 	xxd -iC payload_A9.bin payload_A9.h
 	xxd -iC payload_notA9.bin payload_notA9.h
 	xxd -iC payload_notA9_armv7.bin payload_notA9_armv7.h
