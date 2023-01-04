859a6ae0a18493ae7cd4cc8b7437f6a7c263c8f2
Tom E <guacaplushy@icloud.com>
2023-01-04 12:05:25 -0500

    logging level

--- incl/logging.h
+++ incl/logging.h
@@ -7,11 +7,11 @@
 #define _color_debug "\033[0;94m"
 #define _color_reset "\033[0m"
 
-#define _log_base(str, color, ...) printf(color "%s:%d (%s) --> " str _color_reset, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define _log_base(str, color, level, ...) printf(color "[%s] %s:%d (%s) --> " str _color_reset, level, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
-#define log_debug(str, ...) _log_base(str, _color_debug, ##__VA_ARGS__)
-#define log_error(str, ...) _log_base(str, _color_error, ##__VA_ARGS__)
-#define log_info(str, ...) _log_base(str, _color_info, ##__VA_ARGS__)
-#define log_warn(str, ...) _log_base(str, _color_warn, ##__VA_ARGS__)
+#define log_debug(str, ...) _log_base(str, _color_debug, "Debug", ##__VA_ARGS__)
+#define log_error(str, ...) _log_base(str, _color_error, "Error", ##__VA_ARGS__)
+#define log_info(str, ...) _log_base(str, _color_info, "Info", ##__VA_ARGS__)
+#define log_warn(str, ...) _log_base(str, _color_warn, "Warn", ##__VA_ARGS__)
 
 #endif
